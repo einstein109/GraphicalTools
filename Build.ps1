14eabb393d0832f25ba3b7cbeae17f50927d986f
einstein109 <107047869+einstein109@users.noreply.github.com>
2023-07-11 16:21:04 +0300

    Initial commit

---
+++ Build.ps1
@@ -0,0 +1,13 @@
+# Build script for buildling/testing from the commnad line. See tasks.json for how build is invoked within VS Code
+# GraphicalTools includes two modules: Microsoft.PowerShell.GraphicalTools and Microsoft.PowerShell.ConsoleGuiTools
+# To build them all leave -ModuleName off the `InvokeBuild` command (e.g. Invoke-Build Build).
+# To build only one, specify it using the -ModuleName paramater (e.g. Invoke-Build Build -ModuleName Microsoft.PowerShell.ConsoleGuiTools).
+
+# Build...
+Invoke-Build Build -ModuleName Microsoft.PowerShell.ConsoleGuiTools
+
+# Run what was built as a bit of test of:
+# - Scale: recursive ls of the project
+# - Filter: proving regex works
+# - SelectMultiple
+pwsh -noprofile -command "Import-Module -verbose '$PSScriptRoot/module/Microsoft.PowerShell.ConsoleGuiTools'; Get-ChildItem -Recurse | Out-ConsoleGridView -OutputMode Multiple -Title 'Imported Modules' -Filter \.xml"
